184df367c0f92bbb7780f0458156b8102ba4c38f
mikhalenkadaniil <mikhalenka.d@gmail.com>
2025-10-26 18:29:47 +0100

    feat: resolve remove-from-list task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -21,9 +21,24 @@ const { NotImplementedError } = require('../lib/errors');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function removeKFromList(l, k) {
+  while (l !== null && l.value === k) {
+    l = l.next;
+  }
+
+  let current = l;
+
+  if (!current) return null;
+
+  while (current.next !== null) {
+    if (current.next.value === k) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+
+  return l;
 }
 
 module.exports = {
